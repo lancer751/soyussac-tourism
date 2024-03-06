389b8d0878b8c16d5ef06da8de44613da072abea
lancerlot751 <pachecolau27@gmail.com>
2024-03-06 15:45:32 -0500

    se realizaron pequeños cambios de customizacion y funcionalidad de la pagina

--- src/js/menu-lateral.js
+++ src/js/menu-lateral.js
@@ -2,6 +2,29 @@ const menuLateral = document.querySelector('#menu-lateral');
 const menuOpen = document.querySelector('#menu-open');
 const menuClose = document.querySelector('#menu-close');
 const body = document.querySelector('#body')
+const menuLinks = document.querySelector('#nav-links')
+
+menuLinks.querySelectorAll('a').forEach(linkPage => {
+    linkPage.addEventListener('click', e=>{
+        e.preventDefault();
+        const urlPage = e.target.href;
+        menuLateral.classList.add('-translate-y-full');
+        body.classList.remove('overflow-y-hidden');
+
+        setTimeout(() => {
+        window.location.href = urlPage;
+            
+        }, 300);
+    });
+});
+
+window.addEventListener('resize', e=>{
+    if(window.innerWidth > 768){
+        menuLateral.classList.add('-translate-y-full');
+        body.classList.remove('overflow-y-hidden');
+    }
+})
+  
 
 menuOpen.addEventListener('click', ()=>{
     menuLateral.classList.remove('-translate-y-full');
